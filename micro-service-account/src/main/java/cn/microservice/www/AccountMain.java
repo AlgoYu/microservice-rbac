5da6d7a9694254af7d6f6612ccb245a0d47c471e
XiaoYu <794763733@qq.com>
2020-08-25 10:02:50 +0800

    Add swagger config.

--- micro-service-account/src/main/java/cn/microservice/www/AccountMain.java
+++ micro-service-account/src/main/java/cn/microservice/www/AccountMain.java
@@ -2,12 +2,14 @@ package cn.microservice.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * 账户服务启动类
  * @author MachineGeek
  */
 @SpringBootApplication
+@EnableOpenApi
 public class AccountMain {
     public static void main(String[] args) {
         SpringApplication.run(AccountMain.class,args);
--- micro-service-account/src/main/java/cn/microservice/www/controller/AccountController.java
+++ micro-service-account/src/main/java/cn/microservice/www/controller/AccountController.java
@@ -3,25 +3,51 @@ package cn.microservice.www.controller;
 import cn.microservice.www.common.Result;
 import cn.microservice.www.model.Account;
 import cn.microservice.www.service.impl.AccountServiceImplement;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 账户控制器
  * @author MachineGeek
  */
+@Api(tags = "账户接口")
 @RestController
 @RequestMapping(value = "/account")
 public class AccountController {
     @Autowired
     private AccountServiceImplement accountService;
 
+    @ApiOperation(value = "添加账户")
     @PostMapping(value = "/add")
-    public Result addAccount(@RequestBody Account account){
+    public Result add(@RequestBody Account account){
         accountService.save(account);
         return Result.success(account.getId());
+    }
+
+    @ApiOperation(value = "删除账户")
+    @DeleteMapping(value = "/deleteById")
+    public Result deleteById(long id){
+        accountService.removeById(id);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "修改账户")
+    @PutMapping(value = "/modifyById")
+    public Result modifyById(@RequestBody Account account){
+        return Result.success(accountService.updateById(account));
+    }
+
+    @ApiOperation(value = "获取账户")
+    @GetMapping(value = "/getById")
+    public Result getById(long id){
+        return Result.success(accountService.getById(id));
+    }
+
+    @ApiOperation(value = "获取账户列表")
+    @GetMapping(value = "/getList")
+    public Result getList(){
+        return Result.success(accountService.list());
     }
 }
