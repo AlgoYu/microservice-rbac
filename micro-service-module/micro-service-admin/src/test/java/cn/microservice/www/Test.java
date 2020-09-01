ea2d8fbe5c1b35cbbf94f6f4240879f67be2b2ac
XiaoYu <794763733@qq.com>
2020-09-01 15:58:12 +0800

    Refactoring project.

---
+++ micro-service-module/micro-service-admin/src/test/java/cn/microservice/www/Test.java
@@ -0,0 +1,12 @@
+package cn.microservice.www;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void Test(){
+
+    }
+}
