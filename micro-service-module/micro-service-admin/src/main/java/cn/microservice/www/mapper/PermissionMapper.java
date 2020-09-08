8a45542a5e2da1bf67ecbcc5b2dcd1f3e9f3ee34
machine-geek <794763733@qq.com>
2020-09-08 09:30:39 +0800

    Add generator module.

--- micro-service-module/micro-service-admin/src/main/java/cn/microservice/www/mapper/PermissionMapper.java
+++ micro-service-module/micro-service-admin/src/main/java/cn/microservice/www/mapper/PermissionMapper.java
@@ -3,6 +3,9 @@ package cn.microservice.www.mapper;
 import cn.microservice.www.model.Permission;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 权限映射类
@@ -10,4 +13,5 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface PermissionMapper extends BaseMapper<Permission> {
+    List<Permission> selectPermissionsByRoleId(@Param(value = "roleId") Long roleId);
 }
--- micro-service-module/micro-service-admin/src/main/java/cn/microservice/www/mapper/RoleMapper.java
+++ micro-service-module/micro-service-admin/src/main/java/cn/microservice/www/mapper/RoleMapper.java
@@ -3,6 +3,9 @@ package cn.microservice.www.mapper;
 import cn.microservice.www.model.Role;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 角色映射类
@@ -10,4 +13,5 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
+    List<Role> selectRolesByAccountId(@Param(value = "accountId")Long accountId);
 }
